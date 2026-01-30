72413317cebddeed5139db4c0369096eabbcef39
Roman Strakhov <roman@bestsiteever.net>
2026-01-30 08:27:32 +0400

    add spyfall

---
+++ spy/js/locale.js
@@ -0,0 +1,364 @@
+Glob.currentLocale = 'en';
+
+// map with translations
+Glob.localeMap = {
+    "Spyfall": {
+        "ru": "Находка для шпиона",
+        "nl": "Spyfall",
+        "en": "Spyfall",
+    },
+    "Spyfall_welcome_txt": {
+        "en": "This is a mobile version of the famous Spyfall board game.",
+        "ru": "Перед Вами мобильная версия известной игры \"Находка для шпиона\".",
+        "nl": "Dit is een mobiele versie van het bekende Spyfall bordspel",
+    },
+    "Host new game": {
+        "ru": "Создать новую игру",
+        "nl": "Organiseer een nieuw spel",
+        "en": "Host new game",
+    },
+    "Let's go!": {
+        "ru": "Поехали!",
+        "nl": "Start!",
+        "en": "Let's go!",
+    },
+    "Let all players scan this QR code:": {
+        "ru": "Дайте всем игрокам сканировать этот QR-код:",
+        "nl": "Laat alle spelers deze QR-code scannen:",
+        "en": "Let all players scan this QR code:",
+    },
+    "Or send them this link:": {
+        "ru": "Или поделитесь этой ссылкой на игру:",
+        "nl": "Of stuur ze deze link:",
+        "en": "Or send them this link:",
+    },
+    "Start round 1": {
+        "ru": "Начать первый раунд",
+        "nl": "Start ronde 1",
+        "en": "Start round 1",
+    },
+    "Who are you?": {
+        "ru": "Кто Вы?",
+        "nl": "Wie ben je?",
+        "en": "Who are you?",
+    },
+    "Click on your name and get ready.": {
+        "ru": "Нажмите на своё имя и приготовьтесь.",
+        "nl": "Klik op je naam en maak je klaar.",
+        "en": "Click on your name and get ready.",
+    },
+    "Pick player above": {
+        "ru": "Выберите игрока",
+        "nl": "Kies een speler hierboven",
+        "en": "Pick player above",
+    },
+    "Some statistics": {
+        "ru": "Немного статистики",
+        "nl": "Enkele statistieken",
+        "en": "Some statistics",
+    },
+    "Game over": {
+        "ru": "Конец игры",
+        "nl": "Einde spel",
+        "en": "Game over",
+    },
+    "Main menu": {
+        "ru": "Главное меню",
+        "nl": "Hoofdmenu",
+        "en": "Main menu",
+    },
+    "Player": {
+        "ru": "Игрок",
+        "nl": "Speler",
+        "en": "Player",
+    },
+    "Get ready!": {
+        "ru": "Приготовились!",
+        "nl": "Maak je klaar!",
+        "en": "Get ready!",
+    },
+    "you": {
+        "ru": "Вы",
+        "nl": "jij",
+        "en": "you",
+    },
+    "delete": {
+        "ru": "удалить",
+        "nl": "verwijderen",
+        "en": "delete",
+    },
+    "Who's gonna play?": {
+        "ru": "Кто будет играть?",
+        "nl": "Wie gaat er spelen?",
+        "en": "Who's gonna play?",
+    },
+    "add player": {
+        "ru": "добавить игрока",
+        "nl": "speler toevoegen",
+        "en": "add player",
+    },
+    "copy": {
+        "ru": "копировать",
+        "nl": "kopieer",
+        "en": "copy",
+    },
+    "copied!": {
+        "ru": "скопировано!",
+        "nl": "gekopieerd!",
+        "en": "copied!",
+    },
+    "Game setup corrupted": {
+        "ru": "Невозможно считать параметры игры",
+        "nl": "Spelinstellingen onleesbaar",
+        "en": "Game setup corrupted",
+    },
+    "Show current location": {
+        "ru": "Показать текущую локацию",
+        "nl": "Toon huidige locatie",
+        "en": "Show current location",
+    },
+    "In round #": {
+        "ru": "В раунде №",
+        "nl": "In ronde #",
+        "en": "In round #",
+    },
+    "YOU ARE THE <b>SPY</b> IN THIS ROUND!": {
+        "ru": "ВЫ - <b>ШПИОН</b> В ЭТОМ РАУНДЕ!",
+        "nl": "JIJ IS DE <b>SPION</b> IN DEZE RONDE!",
+        "en": "YOU ARE THE <b>SPY</b> IN THIS ROUND!",
+    },
+    "Finish game": {
+        "ru": "Закончить игру",
+        "nl": "Einde spel",
+        "en": "Finish game",
+    },
+    "Finish round ": {
+        "ru": "Закончить раунд ",
+        "nl": "Einde ronde ",
+        "en": "Finish round ",
+    },
+    "Finish the game?": {
+        "ru": "Закончить игру?",
+        "nl": "Stop het spel?",
+        "en": "Finish the game?",
+    },
+    "Start round ": {
+        "ru": "Начать раунд ",
+        "nl": "Start ronde ",
+        "en": "Start round ",
+    },
+    "All possible game locations:": {
+        "ru": "Все возможные локации игры:",
+        "nl": "Alle mogelijke spellocaties:",
+        "en": "All possible game locations:",
+    },
+    "Start round 1 as ": {
+        "ru": "Начать первый раунд за ",
+        "nl": "Start ronde 1 als ",
+        "en": "Start round 1 as ",
+    },
+    "When everyone entered the game and selected their names, you can start the game. Good luck!": {
+        "ru": "Когда все зашли в игру и выбрали свои имена, Вы можете начинать. Удачи!",
+        "nl": "Wanneer alle spelers ingelogd zijn en de namen van de spelers geselecteerd zijn, kunt u het spel beginnen. Veel success!",
+        "en": "When everyone entered the game and selected their names, you can start the game. Good luck!",
+    },
+    "Round": {
+        "ru": "Раунд",
+        "nl": "Ronde",
+        "en": "Round",
+    },
+    "Making QR code...": {
+        "ru": "Создаю QR-код...",
+        "nl": "QR-code genereren...",
+        "en": "Making QR code...",
+    },
+    "Can't generate QR code _reason_": {
+        "ru": "Невозможно сгенерировать QR-код. Пожалуйста, вернитесь назад и впишите имена игроков латинницей, либо используйте эту ссылку, чтобы поделиться игрой:",
+        "en": "Can't generate QR code. Please go back and write player names with Latin letters only, or use the URL below to share:",
+        "nl": "Kan geen QR-code genereren. Gelieve terug te gaan en de spelernaam invoegen met alleen Latijnse letters, of gebruik de volgende URL om het spel te delen:",
+    },
+    "Spy": {
+        "ru": "Шпион",
+        "nl": "Spion",
+        "en": "Spy",
+    },
+    "Location": {
+        "ru": "Локация",
+        "nl": "Locatie",
+        "en": "Location",
+    },
+    "There should be at least 3 players": {
+        "ru": "Должны играть как минимум 3 игрока",
+        "nl": "Minimaal 3 spelers moeten spelen",
+        "en": "There should be at least 3 players",
+    },
+    "The civillians": {
+        "ru": "Мирные жители",
+        "nl": "De burgers",
+        "en": "The civillians",
+    },
+    "Who won this round?": {
+        "ru": "Кто выиграл раунд?",
+        "nl": "Wie heeft deze ronde gewonnen?",
+        "en": "Who won this round?",
+    },
+    "Winners": {
+        "ru": "Победители",
+        "nl": "Winnaars",
+        "en": "Winners",
+    },
+    "New game": {
+        "ru": "Новая игра",
+        "nl": "Nieuw spel",
+        "en": "New game",
+    },
+    "Night mode": {
+        "ru": "Ночной режим",
+        "nl": "Nachtmodus",
+        "en": "Night mode",
+        
+    },
+    "Language": {
+        "ru": "Язык",
+        "nl": "Taal",
+        "en": "Language",
+    },
+    "Start over again?": {
+        "ru": "Начать сначала?",
+        "nl": "Nog een keer?",
+        "en": "Start over again?",
+    },
+    "Leave this game?": {
+        "ru": "Покинуть текущую игру?",
+        "nl": "Verlaat dit spel?",
+        "en": "Leave this game?",
+    },
+    "How to play?": {
+        "ru": "Как играть?",
+        "nl": "Hoe te spelen?",
+        "en": "How to play?",
+    },
+    "Number of rounds": {
+        "ru": "Количество раундов",
+        "nl": "Aantal rondes",
+        "en": "Number of rounds",
+
+    },
+    "Locations pack": {
+        "ru": "Набор локаций",
+        "nl": "Locatiespakket",
+        "en": "Locations pack",
+    },
+    "More settings": {
+        "ru": "Дополнительные настройки",
+        "nl": "Meer instellingen",
+        "en": "More settings",
+    },
+    "": {
+        "ru": "",
+        "nl": "",
+        "en": "",
+    },
+};
+
+//tr("When everyone entered the game and selected their names, you can start the game. Good luck!")
+function tr(w) {
+    if (!Glob.localeMap.hasOwnProperty(w)) {
+        console.warn("no locale thing for " + w);
+        return w;
+    }
+    var options = Glob.localeMap[w];
+    if (Glob.currentLocale === 'ru' && options.hasOwnProperty(Glob.currentLocale)) {
+        return options[Glob.currentLocale];
+    }
+    // else consider the word in Dutch
+    if (Glob.currentLocale === 'nl' && options.hasOwnProperty('nl')) {
+        return options['nl'];
+    }
+    // else consider the word in English
+    if (options.hasOwnProperty('en'))
+        return options['en'];
+
+    return options['en'];
+}
+// rules
+// Glob - global module with properties and functions that are used everywhere in the game
+// It is created by the game when it starts, and is used to store the game setup and state
+// and to provide functions to work with the game setup and state
+Glob.localeMap["game_rules"] = {
+    "ru": `
+<h2>Что это?</h2>
+<b>Находка для шпиона</b> - психологическая игра, цель которой - раскрыть шпиона. Оптимально играть компанией 4-5 человек.
+
+<h2>Как играть?</h2>
+1. Соберите компанию игроков. Да, вы все должны собраться в реальном мире.<br>
+2. Создайте новую игру. Впишите имена всех, кто будет играть, начиная с себя.<br>
+3. Когда все остальные игроки сканировали QR-код игры, можно начинать Раунд.<br>
+
+<h2>Раунд игры</h2>
+Каждый раунд проходит на определённой локации. Она известна всем игрокам (кнопка "показать локацию"), кроме шпиона. Игроки по кругу задают друг другу вопросы про локацию, пытаясь выяснить, кто из них шпион.
+
+<h2>Задача мирных жителей</h2>
+Задача мирных - вычислить шпиона с помощью вопросов.<br>
+Допустим, раунд проходит на локации "Космический корабль". Вы задаёте вопрос соседy - игроку X: "ты был на этой локации?". Если игрок X отвечает "Да", что вряд ли является правдой и наводит подозрения, можно предложить всем устроить <b>голосование</b> против игрока X. Если большинство Вас поддержало в голосовании, и оказывается, что игрок X действительно шпион, то все <b>мирные выигрывают раунд</b>, получают по 1 очку и начинается следующий раунд. Если же игрок X оказался мирным, то раунд <b>выигрывает шпион</b> и получает за него 2 очка, а мирные - ни одного.<br><br>
+Вопросы и ответы на них <b>не должны быть слишком очевидными</b>. К примеру, если на локации "самолёт" на вопрос "что делают люди в этой локации" Вы ответите "летят из одной страны в другую", шпион сразу поймёт, о чём речь. Но если вы ответите "сидят", то, напротив, наведёте подозрения на себя, так как дали слишком общий ответ.
+
+<h2>Задача шпиона</h2>
+<b>Шпион выигрывает</b> раунд в двух случаях: если <b>назовёт вслух локацию</b> раунда или если остальные игроки <b>проголосовали против мирного</b>.
+Сделать попытку назвать вслух локацию можно только один раз. Если она окажется неверной, раунд <b>выигрывают мирные</b>.
+
+<h2>Окончание раунда</h2>
+На раунд даётся фиксированное время (следит ведущий). По его окончании игроки обязаны устроить голосование, а шпион уже не имеет право назвать локацию. Игрок, против которого набралось наибольшее количество голосов, раскрывает свою роль и тем самым определяет, кто выиграл в этом раунде.
+`,
+"en": `
+<h2> What is this? </h2>
+<b> Spyfall </b> is a psychological game which goal is to uncover a spy. It is preferred to play in a company of 4-5 people.
+
+<h2> How to play? </h2>
+1. Get together with a company of players. Yes, you really should all get together in the real world. <br>
+2. Create a new game. Enter the names of everyone who will play, starting with yourself. <br>
+3. When all other players have scanned the QR code for the game, you can start the Round. <br>
+
+<h2> Round of the game </h2>
+Each round takes place at a specific location. It is known to all players (the "show location" button), except for the spy. One by one, players ask each other questions about the location, trying to find out who of them is a spy.
+
+<h2> The "civilians" </h2>
+The task of the "civilians" is to find the spy by asking each other questions. <br>
+Let's suppose the location for this round is the SPACECRAFT. You ask your neighbor - to player X: "have you been to this location before?" If player X answers “Yes”, which is hardly true and causes suspicions, you can initiate <b>a vote</b> against player X. If most of the players supported the vote, and it turns out that player X is indeed a spy, then <b>the "civilians" win the round</b>, each earning 1 point and the next round begins. If player X turned out to be civilian, then <b>the round  is won by the spy</b> who gets 2 points.<br> <br>
+Questions and answers <b>should not be too obvious</b>. For example, if at the location “airplane” to the question “what do people do in this location“ you answer “fly from one country to another”, the spy will immediately understand the location. On the contrary, if you answer "sitting", then you will raise suspicions on yourself, since your answer was way too general.
+
+<h2>The spy</h2>
+<b>The spy wins a round </b> in two cases: if he <b>names the location out loud</b>, or if other players <b>voted against the civilian</b>.
+You can only try to name a location aloud once. If it turns out to be wrong, the round is <b>won by the civilians</b>.
+
+<h2>End of the round</h2>
+For each round, a fixed amount of time is given (the host should watch after it). At its end, players are required to arrange a vote, and the spy no longer has the right to name the location aloud. The player having the largest number of votes against him reveals his role, thus determining who won in this round.
+`, 
+"nl": `
+<h2> Wat is dit? </h2>
+<b> Spyfall </b> is een psychologisch spel waarbij spelers moeten achterhalen wie de spion is. Dit spel is aangeraden te spelen met een groep van 4-5 spelers.
+
+<h2> Hoe moet je dit spelen? </h2>
+1. Kom samen met een groep speler. Ja, in de echte wereld, fysiek bij elkaar. <br>
+2. Maak een nieuw spel. Voer de namen van alle spelers in, waaronder jouw naam. <br>
+3. Wanneer alle andere spelers de QR-code hebben gescand, kan je het spel starten. <br>
+
+<h2> Ronde in het spel </h2>
+Elke ronde speelt zich af op een locatie. Het is bepaalbaar voor alle burgers (de "Toon locatie" knop), behalve voor de spion. Een bij een stellen spelers vragen aan elkaar over de locatie, om uit te vinden wie de spion is.
+
+<h2> De "burgers" </h2>
+De taak van de "burgers" is de spion te vinden door elkaar vragen te stellen. <br>
+Laten we doen alsof deze ronde de locatie het RUIMTESTATION is. Je vraagt je buurman - aan speler X: "Ben jij hier eerder geweest?" Als speler X antwoord Ja, wat waarschijnlijk niet waar is, kun je <b>een stem</b> indienen tegen speler X. Als de meeste spelers ook een stem uitbrengen tegen speler X en speler X is inderdaad een spion, dan winnen <b>de "burgers" de ronde</b>, elke burger krijgt 1 punt en de volgende ronde begint. Als speler X een burger is, dan <b>de ronde is gewonnen door de spion</b> die dan 2 punten krijgt. <br> <br>
+Vragen en antwoorden <b>zouden niet te duidelijk zijn</b>. Bijvoorbeeld voor de locatie "luchthaven” met de vraag “wat doen mensen op deze locatie?“ en jij antwoord “vliegen van het ene land naar het andere”, dan weet de spion gelijk de locatie. Aan de andere kant, Als je antwoord "zitten", dan zul je verdenkingen op jezelf krijgen, omdat je antwoord veel te onspecifiek is.
+
+<h2>De spion</h2>
+<b>De spion wint de ronde </b> in twee situaties: als hij <b>de locatie hardop vertelt</b>, of als andere spelrs <b>een burger eruit hebben gestemt</b>.
+Je kan alleen een locatie één keer benoemen. Als de locatie fout is, dan is de ronde <b>gewonnen door de burgers</b>.
+
+<h2>Einde van een rond</h2>
+Voor elke ronde is een vast aantal tijd gegeven (de spelhost moet de tijd bewaken). Aan het einde moeten spelers een stem uitbrengen, en de spion heeft niet langer het recht om de locatie hardop te zeggen. De speler met de meeste stemmen tegen hem verteld zijn rol, en daarbij de winaar(s) van deze ronde verklaren.
+
+`
+};
+
+
